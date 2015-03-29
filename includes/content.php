b101b92c7bd35ab53b1cdd8aa94d132067d0d7eb
LJWatson <tink@tink.co.uk>
2015-03-29 20:03:39 +0100

    Added schema.org metadata Added properties for CreativeWork.

--- includes/content.php
+++ includes/content.php
@@ -8,10 +8,10 @@
  */
 ?>
 
-<article aria-labelledby="aria-article-<?php the_ID(); ?>" class="article<?php if ( is_sticky() ) : ?> article--sticky<?php endif; ?>" role="article">
+<article aria-labelledby="aria-article-<?php the_ID(); ?>" class="article<?php if ( is_sticky() ) : ?> article--sticky<?php endif; ?>" itemscope itemtype="http://http://schema.org/BlogPosting"  role="article">
     <header>
         <?php if ( is_single() ) : ?>
-            <h1 class="article_title" id="aria-article-<?php the_ID(); ?>"><?php the_title(); ?></h1>
+            <h1 class="article_title" id="aria-article-<?php the_ID(); ?>" itemprop="name"><?php the_title(); ?></h1>
         <?php else : ?>
             <h2 class="article_title" id="aria-article-<?php the_ID(); ?>"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
         <?php endif; // is_single() ?>
--- includes/metadata.php
+++ includes/metadata.php
@@ -12,5 +12,5 @@
     <span class="hide">Posted in </span>
     <?php the_category(' '); ?>
     <span class="hide"> on </span>
-    <time datetime="<?php the_time('Y-m-d') ?>"><?php the_time('F jS Y') ?></time>
+    <time itemprop="datePublished" datetime="<?php the_time('Y-m-d') ?>"><?php the_time('F jS Y') ?></time>
 </p><!-- .metadata -->
